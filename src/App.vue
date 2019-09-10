dd176ea7967b1705c6de55df57c933e6263e5533
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2019-09-09 23:56:21 -0400

    data on the page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,58 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <Games v-bind:games="games"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Games from './components/Games.vue'
+// import gameData from '../assets/data/gameData.json'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Games
+  },
+  data() {
+    return {
+      
+      //hardcoded
+      // games: [
+      //   {
+      //     id: 1,
+      //     homeTeam: "New York Jets",
+      //     awayTeam: "Cleveland Browns",
+      //     location: "MetLife Stadium - East Rutherford, New Jersey",
+      //     //TODO hard coded date and time for now, update later
+      //     date: "Monday, 9/16",
+      //     time: "8:15 PM EST",
+      //     //TODO add weather via weather API
+      //     weather: "TBD"
+      //   },
+      //   {
+      //     id: 2,
+      //     homeTeam: "Carolina Panthers",
+      //     awayTeam: "Tampa Bay Buccaneers",
+      //     location: "Bank of America - Charlotte, North Carolina",
+      //     //TODO hard coded date and time for now, update later
+      //     date: "Thursday, 9/12",
+      //     time: "8:20 PM EST",
+      //     //TODO add weather via weather API
+      //     weather: "TBD"
+      //   },
+      //   {
+      //     id: 3,
+      //     homeTeam: "Baltimore Ravens",
+      //     awayTeam: "Arizona Cardinals",
+      //     location: "M&T Bank Stadium - Baltimore, Maryland",
+      //     //TODO hard coded date and time for now, update later
+      //     date: "Sunday, 9/15",
+      //     time: "1:00 PM EST",
+      //     //TODO add weather via weather API
+      //     weather: "TBD"
+      //   }
+      // ]
+    }
   }
 }
 </script>
---
+++ src/components/Games.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>NFL Weekly Winners</h1>
+    <h3>Select the winners for this week's games!</h3>
+    <div v-bind:key="games.id" v-for="games in gameData">
+      <h3>{{games[0]}}</h3>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import gameData from "../assets/data/gameData.json"
+
+export default {
+  name: 'Games',
+  props: ["games"],
+  data(){
+    return {
+      gameData 
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
