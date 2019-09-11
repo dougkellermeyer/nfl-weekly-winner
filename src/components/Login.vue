fb8c858d0042227bd41f02e1b290044bdcb1fc50
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2019-09-10 23:17:16 -0400

    added login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,70 @@
 <template>
     <div id ="login">
         <h1>Login card goes here</h1>
+
+<v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
         <router-link to="/games">Games</router-link>
     </div>
 
 </template>
 
 <script>
+
 export default {
-    name: 'Login'
+    name: 'Login',
+    data: () => ({
+            valid: false,
+            firstname: '',
+            lastname: '',
+            nameRules: [
+                v => !!v || 'Name is required'
+            ],
+            email: '',
+            emailRules: [
+                v => !!v || 'Email is required',
+                v => v.length >= 1 || 'Email must be valid',
+            ],
+    }),
 }
 </script>
 
